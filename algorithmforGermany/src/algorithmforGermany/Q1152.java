60feac61b0b2848b52b0abf47b468ed90f166fa9
ss <ss@DAA-COM>
2019-05-14 00:09:24 +0900

    [Q1152] 단어의 개수2

--- algorithmforGermany/src/algorithmforGermany/Q1152.java
+++ algorithmforGermany/src/algorithmforGermany/Q1152.java
@@ -1,10 +1,28 @@
 package algorithmforGermany;
 
+import java.util.Scanner;
+
 public class Q1152 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String str;
+		
+		str= sc.nextLine();
+		str = str.trim();
+		int cnt = 0;
+		for(int i = 0; i<str.length();i++) {
+			if(str.charAt(i)==' ') {
+				cnt++;
+			}
+		}
+		if(str.length()!=0&&cnt==0) {
+			System.out.println(1);
+		}
+		else {
+			System.out.println(cnt==0 ? 0 : ++cnt);
+		}
 	}
 
 }
