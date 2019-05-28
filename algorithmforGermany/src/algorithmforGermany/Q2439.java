7f6b31ecbf10f1d47ac955ddafe4b6571f8e1be9
ss <ss@DAA-COM>
2019-05-28 23:31:49 +0900

    [Q2439] 별 찍기 - 2

---
+++ algorithmforGermany/src/algorithmforGermany/Q2439.java
@@ -0,0 +1,25 @@
+package algorithmforGermany;
+
+import java.util.Scanner;
+
+public class Q2439 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = Integer.parseInt(sc.nextLine());
+		
+		for(int i = 1; i<=num;i++) {
+			
+			for(int j1=1; j1<=num-i;j1++) {
+				System.out.print(" ");
+			}
+			for(int j2 = 1; j2<=i;j2++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
