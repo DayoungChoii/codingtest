c9d248038f0ac9632f5aad72e4684963672852da
DayoungChoii <young12430@gmail.com>
2022-05-09 20:23:38 +0900

    [Q68644 프로그래머스] 두개 뽑아서 더하기

---
+++ question/src/programmers/Q68644.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Q68644 {
+    public static void main(String[] args) {
+        solution(new int[] {5,0,2,7});
+    }
+
+    public static int[] solution(int[] numbers) {
+        int[] answer = {};
+        HashSet<Integer> resultSet = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                resultSet.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        answer = resultSet.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        Arrays.sort(answer);
+
+        return answer;
+    }
+}
