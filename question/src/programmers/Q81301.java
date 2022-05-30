0df9136d11e4149432415f7d32a6d55ebadf1620
DayoungChoii <young12430@gmail.com>
2022-05-30 21:35:07 +0900

    [Q81301 프로그래머스] 숫자 문자열과 영단어

---
+++ question/src/programmers/Q81301.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q81301 {
+    public static void main(String[] args) {
+        System.out.println(solution("one4seveneight"));
+    }
+
+    public static int solution(String s) {
+        HashMap<String, String> numbersMap = new HashMap<>();
+        numbersMap.put("zero", "0");
+        numbersMap.put("one", "1");
+        numbersMap.put("two", "2");
+        numbersMap.put("three", "3");
+        numbersMap.put("four","4");
+        numbersMap.put("five","5");
+        numbersMap.put("six","6");
+        numbersMap.put("seven","7");
+        numbersMap.put("eight","8");
+        numbersMap.put("nine","9");
+
+        for (Map.Entry<String, String> entry : numbersMap.entrySet()) {
+            s = s.replace(entry.getKey(), entry.getValue());
+        }
+
+        return Integer.parseInt(s);
+    }
+}
