df29b5a6f45f5d5a82ad75c63cd2fdbe35ccc785
DayoungChoii <young12430@gmail.com>
2022-05-31 21:32:06 +0900

    [Q12922 프로그래머스] 수박수박수박수?

---
+++ question/src/programmers/Q12922.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Q12922 {
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+    }
+
+    public static String solution(int n) {
+        String answer = "";
+        String watermelon = "수박";
+
+       if(n % 2 == 0){
+            answer = watermelon.repeat(n/2);
+        } else{
+            answer = watermelon.repeat(n/2 + 1);
+            answer = answer.substring(0, answer.length() - 1);
+        }
+
+        return answer;
+    }
+}
